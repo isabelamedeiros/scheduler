13bbbc045613dcc1b6facacfe2f7e3a2bf5d9174
isabelamedeiros <smedeirosisabela@gmail.com>
2025-02-23 01:04:33 -0300

    Adicionando o calculo da taxa

--- src/main/java/br/com/scheduler/service/SchedulerService.java
+++ src/main/java/br/com/scheduler/service/SchedulerService.java
@@ -6,7 +6,9 @@ import br.com.scheduler.repository.SchedulerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -27,6 +29,21 @@ public class SchedulerService {
         scheduler.setTransferDate(schedulerDTO.getTransferDate());
         scheduler.setSchedulingDate(LocalDate.now());
 
+        BigDecimal tax = calculateTax(scheduler.getTransferDate(), scheduler.getTransferValue());
+        scheduler.setTax(tax);
+
         return repository.save(scheduler);
     }
+
+    private BigDecimal calculateTax(LocalDate transferDate, BigDecimal transferValue) {
+        long days = ChronoUnit.DAYS.between(LocalDate.now(), transferDate);
+        if (days == 0) return transferValue.multiply(BigDecimal.valueOf(0.025)).add(BigDecimal.valueOf(3.00));
+        if (days >= 1 && days <= 10) return BigDecimal.valueOf(12.00);
+        if (days >= 11 && days <= 20) return transferValue.multiply(BigDecimal.valueOf(0.082));
+        if (days >= 21 && days <= 30) return transferValue.multiply(BigDecimal.valueOf(0.069));
+        if (days >= 31 && days <= 40) return transferValue.multiply(BigDecimal.valueOf(0.047));
+        if (days >= 41 && days <= 50) return transferValue.multiply(BigDecimal.valueOf(0.017));
+        throw new IllegalArgumentException("Data de transferência inválida");
+    }
+
 }
